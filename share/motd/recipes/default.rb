3195e6eeca5f45fdbc8901629ddf936f5f623f7c
Marc Pulido <marc.pulido@gmail.com>
2018-03-19 18:45:36 -0400

    update motd

--- share/motd/recipes/default.rb
+++ share/motd/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+cookbook_file '/etc/motd' do
+  source 'motd'
+  mode '0644'
+end
+
+cookbook_file '/etc/profile.d/motd.sh' do
+  source 'motd.sh'
+  mode '0755'
+end
