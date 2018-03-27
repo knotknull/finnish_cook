35763a4427dd039156d616254e4c786f4823e1ec
knotknull <zulu_mcduffy@yahoo.com>
2018-03-27 16:34:48 -0400

    Composed attribute

--- share/motd_attributes/attributes/default.rb
+++ share/motd_attributes/attributes/default.rb
@@ -1,2 +1,6 @@
 default['motd_attributes']['company'] = "Galacticon Corp"
 default['motd_attributes']['java_bin'] = '/usr/local/bin/java'
+
+default['motd_attributes']['tomcat_home'] = '/usr/local/tomcat6'
+default['motd_attributes']['tomcat_bin'] = "#{node['motd_attributes']['tomcat_home']}/bin/tomcat"
+# /usr/local/tomcat6/bin/tomcat
