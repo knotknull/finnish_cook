df62e49792c4c2661f80526f43d60526069ff4a0
knotknull <zulu_mcduffy@yahoo.com>
2018-03-27 16:56:17 -0400

    Updated tomcat_bin in attributes/tomcat.rb

--- share/motd_attributes/attributes/default.rb
+++ share/motd_attributes/attributes/default.rb
@@ -1,6 +1,6 @@
-default['motd_attributes']['company'] = "Galacticon Corp"
+default['motd_attributes']['company'] = 'Galacticon Corp'
 default['motd_attributes']['java_bin'] = '/usr/local/bin/java'
 
 default['motd_attributes']['tomcat_home'] = '/usr/local/tomcat6'
-default['motd_attributes']['tomcat_bin'] = "#{node['motd_attributes']['tomcat_home']}/bin/tomcat"
+default['motd_attributes']['tomcat_bin'] = "#{node['motd_attributes']['tomcat_bin']}"
 # /usr/local/tomcat6/bin/tomcat
--- share/motd_attributes/attributes/tomcat.rb
+++ share/motd_attributes/attributes/tomcat.rb
@@ -1,2 +1,1 @@
-default['motd_attributes']['tomcat_home'] = '/usr/local/tomcat8'
-default['motd_attributes']['tomcat_bin'] = "#{node['motd_attributes']['tomcat_home']}/bin/tomcat"
+default['motd_attributes']['tomcat_bin'] = "/usr/local/tomcat8/bin/tomcat"
