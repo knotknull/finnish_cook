f1ddc2bc75df84a976126ab8cb2e79da9669c432
knotknull <zulu_mcduffy@yahoo.com>
2018-03-30 18:01:54 -0400

    Attribute driven

--- share/motd_attributes/attributes/default.rb
+++ share/motd_attributes/attributes/default.rb
@@ -4,3 +4,15 @@ default['motd_attributes']['company'] ='The Galacticon Co'
 default['motd_attributes']['tomcat_home'] = '/usr/local/tomcat6'
 default['motd_attributes']['tomcat_bin'] = "#{node['motd_attributes']['tomcat_bin']}"
 # /usr/local/tomcat6/bin/tomcat
+
+default['motd_attributes']['message'] = 'Goovy (not ruby) day'
+
+default['motd_attributes']['motd'] = [
+  "This node is running on #{node['virtualization']['system']}",
+  "This address of this node is #{node['ipaddress']}",
+  node['motd_attributes']['message'],
+  "Welcome to #{node['motd_attributes']['company']}",
+  "JAVA_HOME on this node is #{node['motd_attributes']['java_home']}",
+  "JAVA_BIN  on this node is #{node['motd_attributes']['java_bin']}",
+  "TOMCAT_BIN on this node is #{node['motd_attributes']['tomcat_bin']}",
+]
